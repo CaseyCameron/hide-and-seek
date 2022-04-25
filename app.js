666b4fc828bd9e79e2b234ff6b7158b0ac846312
CaseyCameron <mcasey.cameron@gmail.com>
2022-04-25 12:33:54 -0700

    Finished

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const shedButton = document.getElementById('shed-button');
 const treeButton = document.getElementById('tree-button');
 const boulderButton = document.getElementById('boulder-button');
 
-const shedContainer = document.getElementById('shed-container');
 const treeContainer = document.getElementById('tree-container');
 const boulderContainer = document.getElementById('boulder-container');
+const shedContainer = document.getElementById('shed-container');
+const containers = [shedContainer, treeContainer, boulderContainer];
 
 const totalEl = document.getElementById('total');
 const lossesEl = document.getElementById('losses');
@@ -37,9 +38,30 @@ boulderButton.addEventListener('click', () => {
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
+    containers.map((container) => container.classList.remove('face'));
+
     // then increment the guesses
+    totalGuesses ++;
+
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
+
+    containers.map((container) => {
+        if (container.id === `${correctSpot}-container`) {
+            container.classList.toggle('face');
+        }
+    });
+    
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
+
+    if (userGuess === correctSpot) {
+        correctGuesses ++;
+        winsEl.textContent = `${correctGuesses}`;
+    } else {
+        lossesEl.textContent = `${totalGuesses - correctGuesses}`;
+    }
+    totalEl.textContent = `${totalGuesses}`;
+
+
 }
